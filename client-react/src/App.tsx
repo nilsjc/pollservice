8b93e3b4088c4fb617c83fe6e185b93c8062731e
Nils Edvardsson <nils@nilsjc.com>
2025-03-13 19:49:36 +0100

    added react client

---
+++ client-react/src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Poll tool</h1>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          Create poll
+        </a><br></br>
+        <a href="https://react.dev" target="_blank">
+          Try out poll
+        </a><br></br>
+        <a href="https://vite.dev" target="_blank">
+          Investigate poll
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
