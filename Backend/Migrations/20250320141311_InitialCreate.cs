e3578715534b7be16cd0175cc52de7579a76d45a
Nils Edvardsson <nils@nilsjc.com>
2025-03-23 18:13:43 +0100

    added ef and more

---
+++ Backend/Migrations/20250320141311_InitialCreate.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Polls",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Polls", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    tbl_poll_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    qkey = table.Column<string>(type: "TEXT", nullable: false),
+                    text = table.Column<string>(type: "TEXT", nullable: false),
+                    tbl_pollid = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Questions_Polls_tbl_pollid",
+                        column: x => x.tbl_pollid,
+                        principalTable: "Polls",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Answers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    tbl_user_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    tbl_question_id = table.Column<int>(type: "INTEGER", nullable: false),
+                    value = table.Column<int>(type: "INTEGER", nullable: false),
+                    dateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    tbl_questionid = table.Column<int>(type: "INTEGER", nullable: true),
+                    tbl_userid = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answers", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Answers_Questions_tbl_questionid",
+                        column: x => x.tbl_questionid,
+                        principalTable: "Questions",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_Answers_Users_tbl_userid",
+                        column: x => x.tbl_userid,
+                        principalTable: "Users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_tbl_questionid",
+                table: "Answers",
+                column: "tbl_questionid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_tbl_userid",
+                table: "Answers",
+                column: "tbl_userid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_tbl_pollid",
+                table: "Questions",
+                column: "tbl_pollid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Answers");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Polls");
+        }
+    }
+}
--- Backend/database/PollContext.cs
+++ Backend/database/PollContext.cs
@@ -8,12 +8,28 @@ namespace Backend.database
 {
     public class PollContext : DbContext
     {
-        
+        public DbSet<tbl_poll> Polls { get; set; }
+        public DbSet<tbl_question> Questions { get; set; }
+        public DbSet<tbl_user> Users { get; set; }
+        public DbSet<tbl_answer> Answers { get; set;}
+        public string DbPath { get; }
+        public PollContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "questions03");
+        }
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+
     }
     public class tbl_poll
     {
-        public int id { get; set;}
-        public string name { get; set;}
+        public int id { get; set; }
+        public string name { get; set; }
+        public List<tbl_question> tbl_Questions { get; set; }
     }
     public class tbl_question
     {
@@ -21,16 +37,23 @@ namespace Backend.database
         public int tbl_poll_id { get; set;}
         public string qkey { get; set;}
         public string text { get; set;}
+        public List<tbl_answer> tbl_Answers { get; set; }
     }
 
     public class tbl_user
     {
+        public int id { get; set;}
         public string name { get; set; }
+        public List<tbl_answer> tbl_Answers { get; set; }
     }
 
     public class tbl_answer
     {
         public int id { get; set; }
         public int tbl_user_id { get; set; }
+        public int tbl_question_id { get; set; }
+        public int value { get; set; }
+        public DateTime dateTime { get; set; }
+
     }
 }
